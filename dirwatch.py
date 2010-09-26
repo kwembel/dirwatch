4f90cc66e148d8c7cd7e9efedef16dd58e098fdc
Frederic Boisnard <kwembel@gmail.com>
2010-09-26 20:53:04 +0200

    modified : checks for addition, deletions, modifications

--- dirwatch.py
+++ dirwatch.py
@@ -36,33 +36,61 @@ class DirectoryWatcher():
     self.watcher = QtCore.QFileSystemWatcher()
     self.options = options
     self.files_watched = None
+    self.files_stats = {}
     QObject.connect(self.watcher, SIGNAL("directoryChanged(const QString &)"), self.dirChanged)
     
   def addPath(self, path):
     qpath = QtCore.QString(path)
     self.watcher.addPath(qpath)    
-    
+      
     # limitation : only 1 directory is watched
     self.files_watched = set(os.listdir(self.watcher.directories()[0]))
+    for f in self.files_watched:
+      self.files_stats[f] = os.stat(f)
     
   def dirChanged(self, path):
     new_files_watched = set(os.listdir(self.watcher.directories()[0]))
     
     # Compare the new state of the files in the directory with the previous one
-    
-    files_removed = self.files_watched - new_files_watched 
-    for f in files_removed:
-      print("removed: %s" % f)
-      
+    #
+    # Check 1 : additions
     files_added = new_files_watched - self.files_watched
-    for f in files_added:
-      print("added: %s" % f)      
-    
-    if len(files_added) == 0 and len(files_removed) == 0:
-      print("other change")
+    if self.options.additions:
+      for f in files_added:
+        # Display
+        if self.options.verbose:
+          print("added: %s" % f)      
+        else:
+          print("%s" % f)
+        # Management
+        self.files_watched.add(f)
+        self.files_stats[f] = os.stat(f)
+
+    # Check 2 : deletions
+    files_removed = self.files_watched - new_files_watched 
+    if self.options.deletions:
+      for f in files_removed:
+        # Display
+        if self.options.verbose:
+          print("removed: %s" % f)
+        else:
+          print("%s" % f)
+        # Management
+        self.files_watched.remove(f)
+        del self.files_stats[f]
       
-    # Update the state of the watched files
-    self.files_watched = new_files_watched
+    # Check 3 : Modification (file size changed, permissions, ...)
+    if self.options.modifications:
+      for f in new_files_watched:
+        current_stat = os.stat(f)
+        if self.files_stats[f] != current_stat:
+          # Display
+          if self.options.verbose:
+            print("modified: %s" % f)
+          else:
+            print("%s" % f)
+          # Management
+          self.files_stats[f] = current_stat           
   
 def main():
   import optparse
@@ -73,12 +101,11 @@ def main():
   
   parser = optparse.OptionParser(usage=usage, version=version)
   parser.add_option("-v", "--verbose", 	action="store_true", dest="verbose", 	default=False, 	help="verbose mode")
-  parser.add_option("-a", "--add", 	action="store_true", dest="add", 	default=True, 	help="additions")
-  parser.add_option("-d", "--del", 	action="store_true", dest="delete", 	default=True, 	help="deletions")
-  parser.add_option("-c", "--change", 	action="store_true", dest="change", 	default=True, 	help="changes")
-  parser.add_option("-p", "--permissions", action="store_true", dest="permissions", default=True, help="permissions")
-  parser.add_option("-r", "--recursive", action="store_true",dest="recursive", 	default=True, 	help="recursive")
-  # Add : e for everything
+  parser.add_option("-a", "--add", 	action="store_true", dest="additions", 	default=False, 	help="additions")
+  parser.add_option("-d", "--del", 	action="store_true", dest="deletions", 	default=False, 	help="deletions")
+  parser.add_option("-c", "--modif", 	action="store_true", dest="modifications", 	default=False, 	help="modificaitons")
+  parser.add_option("-e", "--everything", 	action="store_true", dest="everything", 	default=False, 	help="everything")
+  parser.add_option("-r", "--recursive", action="store_true",dest="recursive", 	default=False, 	help="recursive")
   # Add : l for long
   # Add : t for time
   # Add : i for ignore
@@ -95,7 +122,20 @@ def main():
   if not os.path.isdir(dir_path):
       sys.stdout.write("Error: '%s' is not a directory\n" % dir_path)
       return 1
-        
+  
+  # If no check is asked, activate 'everything' by default
+  if not options.everything \
+    and not options.additions \
+    and not options.deletions \
+    and not options.modifications:
+      options.everything = True
+      
+  # 'everything' option is choosen : activate every checks
+  if options.everything:
+    options.additions = True
+    options.deletions = True
+    options.modifications = True
+  
   # Creation of the Qt Context
   app = QCoreApplication(sys.argv)
   
